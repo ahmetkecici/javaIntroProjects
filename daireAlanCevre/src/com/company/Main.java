470c2abb3ae1751e8194d96a46bed98798c0a1cc
ahmetkecici 99 <ahmetkecici99@gmail.com>
2021-11-21 15:42:21 +0300

    daire çevre alan hesaplandı

---
+++ daireAlanCevre/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int yaricap,aci;
+        double pi=3.14;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Dairenin yarıçapını giriniz");
+
+
+        yaricap=scanner.nextInt();
+        System.out.println("Dairenin merkez açısını giriniz");
+
+
+        aci=scanner.nextInt();
+        double alan=pi*yaricap*yaricap;
+        double cevre=2*pi*yaricap;
+        alan=(pi*(yaricap*yaricap)*aci)/360;
+        System.out.println("çevre :"+cevre);
+        System.out.println("alan :"+alan);
+     
+
+    }
+}
